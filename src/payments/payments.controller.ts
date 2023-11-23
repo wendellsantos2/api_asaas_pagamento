a995edf7c13a5a81bd392f8c2da75a76af48cb9d
wendellsantos2 <wemdellcos@gmail.com>
2023-11-23 18:09:35 -0300

    feat : create cliente

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -3,6 +3,26 @@ import { ApiTags} from '@nestjs/swagger';
 import { PaymentService } from './payments.service';
  
 
+export class CreateUserDTO {
+  readonly name: string;
+  readonly cpfCnpj: string;
+  readonly email: string;
+  readonly phone: string;
+  readonly address: string;
+  readonly addressNumber: string;
+  readonly province: string;
+  readonly postalCode: string;
+  readonly mobilePhone: string;
+  readonly complement: string;
+  readonly externalReference: string;
+  readonly notificationDisabled: boolean;
+  readonly additionalEmails: string;
+  readonly stateInscription: string;
+  readonly municipalInscription: string;
+  readonly observations: string;
+}
+
+
 export class SaleCreateLinkPaymentDTO {
     
     billingType: string;
@@ -37,4 +57,15 @@ export class PaymentsController {
           throw new BadRequestException(error.message);
       }
   }
+
+  @Post('create-client')
+  async createCliente(@Body() createUserDTO: CreateUserDTO) {
+
+    try {
+        const result = await this.paymentService.createCliente(createUserDTO);
+        return result;
+    } catch (error) {
+        throw new BadRequestException(error.message);
+    }
+}
 }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,11 +1,13 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { SaleCreateLinkPaymentDTO } from './payments.controller';
+import { CreateUserDTO, SaleCreateLinkPaymentDTO } from './payments.controller';
 
 @Injectable()
 export class PaymentService {
+    
     private httpClient: AxiosInstance;
 
+  
     constructor() {
         this.httpClient = axios.create({
             baseURL: 'https://api.asaas.com/v3/', // Base URL da API
@@ -17,6 +19,8 @@ export class PaymentService {
         });
     }
 
+
+   
     // Método para criar um link de pagamento através de uma API externa
     async createPaymentLink(saleCreateLinkPaymentDTO: SaleCreateLinkPaymentDTO): Promise<any> {
         try {
@@ -43,6 +47,29 @@ export class PaymentService {
             } else {
                 // Erro genérico
                 throw new BadRequestException('Failed to create the payment link.');
+            }
+        }
+    }
+
+    async createCliente(createUserDTO: CreateUserDTO): Promise<any> {
+        try {
+            // Construindo o payload com base no DTO
+            const payload = {
+                name: createUserDTO.name,
+                cpfCnpj: createUserDTO.cpfCnpj,
+                email: createUserDTO.email,
+                // Adicione outros campos do DTO conforme necessário
+            };
+
+            const response = await this.httpClient.post('customers', payload); // Faz a requisição POST para 'api/v3/customers'
+            return response.data; // Retorna os dados da resposta
+        } catch (error) {
+            if (error.response) {
+                // Captura erros específicos da resposta da API
+                throw new BadRequestException(`API Error: ${error.response.status} - ${error.response.data}`);
+            } else {
+                // Erro genérico
+                throw new BadRequestException('Failed to create the customer.');
             }
         }
     }
